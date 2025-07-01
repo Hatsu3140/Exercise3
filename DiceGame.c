6340f56eba981c5983a517e5dd8dfe85a4b7f59e
Hatsu3140 <167418869+Hatsu3140@users.noreply.github.com>
2025-07-01 19:45:15 +0900

    Dice game: initial revision

---
+++ DiceGame.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(void){
+    int dice1, dice2, sum;
+
+    srand((unsigned int)time(NULL)); 
+    dice1 = (rand() % 6) + 1;
+    dice2 = (rand() % 6) + 1;
+    sum = dice1 + dice2;
+
+    printf("Rolling dice...\nDie 1: %d\nDie 2: %d\nTotal value: %d\n", dice1, dice2, sum);
+}
